f38eb190c89920e7529d57260adfa5d98df095a0
edzapodka <edzapodka@gmail.com>
2018-02-25 13:00:50 +0700

    Initial commit

---
+++ app/src/main/java/e74studio/dinnerdice/MainActivity.kt
@@ -0,0 +1,30 @@
+package e74studio.dinnerdice
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("nasi goreng", "kwetiaw", "mie baso", "hamburger", "mie goreng")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        decideBtn.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+
+            selectedFoodTxt.text = foodList[randomFood]
+        }
+
+        addFoodBtn.setOnClickListener {
+            val newFood = addFoodTxt.text.toString()
+            foodList.add(newFood)
+            addFoodTxt.text.clear()
+            println(foodList)
+        }
+
+    }
+}
